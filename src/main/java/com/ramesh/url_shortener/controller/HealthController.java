5cdfd913f6fbbdb63d529a74838427dedb1ebcfc
Ramesh Babu <ramvirat2020@gmail.com>
2026-06-14 22:48:02 +0530

    feat: dockerize application

--- src/main/java/com/ramesh/url_shortener/controller/HealthController.java
+++ src/main/java/com/ramesh/url_shortener/controller/HealthController.java
@@ -1,12 +1,14 @@
 package com.ramesh.url_shortener.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/health")
 public class HealthController {
 
-    @GetMapping("/health")
+    @GetMapping
     public String health()
     {
         return "Application Running";
